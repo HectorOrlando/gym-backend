4b1c9dc1c95d1a1a8896b7f72ef1f02a6b8abddd
HectorOrlando <14orgui@gmail.com>
2023-11-20 14:04:38 +0100

    Implement skeleton

---
+++ src/domain/repositories/UserRepository.ts
@@ -0,0 +1,9 @@
+// UserRepository.ts
+
+import { WithId, Document } from "mongodb";
+
+// Interfaz que define los métodos que debe implementar un repositorio de usuarios.
+export interface UserRepository {
+    findAllUsers(): Promise<WithId<Document>[]>;
+    findUserById(id:string): Promise<WithId<Document>>;
+}
